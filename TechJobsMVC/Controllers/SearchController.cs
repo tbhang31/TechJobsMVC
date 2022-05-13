e270bf3a64da56ddb704260a83329443572df503
Tom Hang <tbhang31@gmail.com>
2022-05-13 19:31:19 -0400

    I got lazy and made an extra Results.cshtml page

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +16,26 @@ namespace TechJobsMVC.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            return View();
+        }
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs = new List<Job>();
+            if (searchTerm == "" || searchTerm == null)
+            {
+                jobs = JobData.FindAll();
+            }
+            else
+            {
+                jobs=JobData.FindByColumnAndValue(searchType, searchTerm);
+                if (jobs.Count == 0)
+                {
+                    jobs = null;
+                }
+            }
+            
+            ViewBag.searchedJobs = jobs;
             return View();
         }
 
